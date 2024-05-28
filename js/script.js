816600aba27b9d3c7f59d723c08829a3aee4d738
idriss112 <idrisslaaziri@gmail.com>
2024-05-28 09:40:30 -0400

    ready

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 var countDownDate = new Date();
-countDownDate = new Date(countDownDate.getFullYear() +1, countDownDate.getMonth() -6, 1);
+countDownDate = new Date(countDownDate.getFullYear() + 1, countDownDate.getMonth() - 6, 1);
 
-var x = setInterval(function(){
+var x = setInterval(function () {
     var now = new Date();
     var diff = countDownDate - now;
 
-    var months = Math.floor(diff /(1000*60*60*24*30));
-    var days = Math.floor((diff % (1000*60*60*24*30) )/ (1000*60*60*24));
-    var hours = Math.floor((diff % (1000*60*60*24) )/ (1000*60*60));
-    var minutes = Math.floor((diff % (1000*60*60) )/ (1000*60));
-    var seconds = Math.floor((diff % (1000*60) )/ 1000);
+    var months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
+    var days = Math.floor((diff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
     document.getElementById("months").innerHTML = months;
     document.getElementById("days").innerHTML = days;
@@ -17,3 +17,28 @@ var x = setInterval(function(){
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 }, 1000)
+
+
+
+
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function () {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    })()
